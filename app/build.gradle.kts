fb2513929fef404064bde6f9eaf6a21e39cc872e
joey0293 <225192438+joey0293@users.noreply.github.com>
2026-05-22 06:02:22 -0400

    Bump version to v1.3.2 (versionCode 29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tristinbaker.idlefantasy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.3.1"
+        versionCode = 29
+        versionName = "1.3.2"
     }
 
     dependenciesInfo {
